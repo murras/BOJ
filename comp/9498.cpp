1e837589c12e56d56e7c767835f3ea0954a711a1
murras <gud2169@naver.com>
2018-03-19 00:35:47 +0900

    18.03.19 문제풀이 및 데이터파일 삭제

---
+++ comp/9498.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    if (n >= 90 && n <= 100)
+    {
+        cout << 'A' << '\n';
+    }
+    else if (n >= 80 && n <= 89)
+    {
+        cout << 'B' << '\n';
+    }
+    else if (n >= 70 && n <= 79)
+    {
+        cout << 'C' << '\n';
+    }
+    else if (n >= 60 && n <= 69)
+    {
+        cout << 'D' << '\n';
+    }
+    else
+    {
+        cout << 'F' << '\n';
+    }
+}
