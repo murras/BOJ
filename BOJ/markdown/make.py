24a9ab4d39949d555ab3c36912cd37e6a529ead0
murras <gud2169@naver.com>
2018-11-26 21:34:28 +0900

    18 11 26

--- BOJ/markdown/make.py
+++ BOJ/markdown/make.py
@@ -3,14 +3,25 @@ pNum = sys.argv[1]
 pNameArg = sys.argv[2:]
 pName = ""
 
-for i in range(len(pNameArg)) :
+for i in range(len(pNameArg)):
     pName += str(pNameArg[i])
     pName += " "
 
-fileName = "BOJ 백준 " + str(pNum) + "번 " + pName +"C++.md"
-content = "## [BOJ " + str(pNum) + "번 " + pName + "문제](https://www.acmicpc.net/problem/" + pNum+ ")\n\n\n"
-content += "> " + pNum + ".cpp\n\n"
-content += "```cpp\n\n```"
 
-with open(fileName, "w") as f :
-    f.write(content)
+fileName = "BOJ 백준 " + str(pNum) + "번 " + pName + "C++.md"
+
+content = "## [BOJ " + str(pNum) + "번 " + pName + \
+    "문제](https://www.acmicpc.net/problem/" + pNum + ")\n\n\n\n"
+content += "백준 " + str(pNum) + "번 : " + pName + " C++\n"
+content += "> " + pNum + ".cpp\n\n"
+content += "```cpp\n"
+
+with open(pNum+".cpp", "r") as f:
+    lines = f.readlines()
+    for line in lines:
+        content += line
+content += "\n```"
+
+
+with open(fileName, "w") as f:
+    f.write(str(content))
--- BOJ/push/1000-4999/1927.cpp
+++ BOJ/push/1000-4999/1927.cpp
@@ -1,24 +1,23 @@
-// STL 3 p.82
-#include <iostream>
-#include <queue>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
     priority_queue<int, vector<int>, greater<int> > pq;
     int n;
     cin >> n;
-    while(n--){
-        int x;
-        cin >> x;
-        if(x == 0){
-            if(pq.empty()){
-                cout << 0 << endl;
-            }
-            else {
-            cout << pq.top() << endl;
-            pq.pop();
+    while (n--) {
+        int input;
+        cin >> input;
+        if (input != 0) {
+            pq.push(input);
+        } else {
+            if (pq.empty()) {
+                cout << "0" << '\n';
+            } else {
+                cout << pq.top() << '\n';
+                pq.pop();
             }
         }
-        else
-            pq.push(x);
     }
 }
