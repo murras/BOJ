0c3c3667fe976450efeb51d14b4f53e1d395b16b
Hyeongseok Kim <gud2169@naver.com>
2018-12-01 19:36:08 +0900

    2018 12 01 posting and script

---
+++ BOJ/markdown/done.bash
@@ -0,0 +1,1 @@
+python end.py $1
---
+++ BOJ/markdown/end.py
@@ -0,0 +1,23 @@
+import os
+import sys
+myName = "end.py"
+myNameLength = len(myName)
+absPath = os.path.abspath(myName)
+parentDir = (absPath[:-myNameLength])
+parentDirList = os.listdir(parentDir)
+
+for filename in parentDirList:
+    if filename.endswith("html"):
+        print("remove file : " + filename)
+        os.remove(filename)
+    if filename.endswith("md"):
+        print("move file " + filename)
+        os.system("mv \""+filename + "\" done")
+
+num = int(sys.argv[1])
+if num < 5000:
+    os.system("mv " + sys.argv[1] + ".cpp ../push/1000~4999")
+elif num < 10000:
+    os.system("mv " + sys.argv[1] + ".cpp ../push/5000~9999")
+else:
+    os.system("mv " + sys.argv[1] + ".cpp ../push/10000~19999")
