b8bbfc4231536ee5213b236f015943be453f5db8
murras <gud2169@naver.com>
2018-12-23 05:03:44 +0900

    SDS

---
+++ KOITP/2019_SDS/SDS_Lv1_카드단어.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    int test, n;
+    int i = 0;
+    cin >> test;
+    char c;
+    while (test--) {
+        string result = "";
+        cin >> n;
+        i++;
+        while (n--) {
+            cin >> c;
+            if (result[0] > c) {
+                result = result + c;
+            } else {
+                result = c + result;
+            }
+        }
+        cout << "#" << i << " " << result;
+        if (test != 0) cout << '\n';
+    }
+    return 0;
+}
