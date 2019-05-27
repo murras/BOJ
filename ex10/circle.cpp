001c8d021608782050c5d49cc6c213295669a88d
murra <gud2169@naver.com>
2019-05-27 20:41:21 +0900

    0527

--- ex10/circle.cpp
+++
@@ -1,9 +0,0 @@
-#include "circle.h"
-
-CCircle::CCircle(int a, int b, double r) : CShape(a, b), Radius(r) {}
-double CCircle::GetArea() {
-    return (3.14 * Radius * Radius);
-}
-void CCircle::Print() {
-    cout << GetArea() << endl;
-}
